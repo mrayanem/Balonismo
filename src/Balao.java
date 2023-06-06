d660719a5bb8589c11563a5026c24a4b8528f947
mrayanem <mmarquesraioane586@gmail.com>
2023-06-06 12:10:17 -0300

    att

--- src/Balao.java
+++ src/Balao.java
@@ -1,5 +1,20 @@
 public class Balao {
 
-    
+    //atributos
+
+    int id;
+    String registro;
+    String cor;
+    String tipo;
+    double altura;
+    double peso;
+    String porte;
+    int capacidadePessoas;
+
+    //metódos
+
+    public String voar(Combustivel gasPropano){
+        return "voando com" + gasPropano;
+    }
 
 }
---
+++ src/Combustivel.java
@@ -0,0 +1,9 @@
+public class Combustivel {
+
+    //atrubutos
+
+    String componentes;
+    double precoLitro;
+    double precoQuilo;
+    String nome;
+}
